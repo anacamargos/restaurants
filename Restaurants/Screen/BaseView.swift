7c56603136f6fc5c781e2fc4386c49fab4af244b
Ana <alcvlara@sga.pucminas.br>
2019-02-23 13:08:25 -0300

    Creating Details Group

---
+++ Restaurants/Screen/BaseView.swift
@@ -0,0 +1,26 @@
+//
+//  BaseView.swift
+//  Restaurants
+//
+//  Created by Ana Leticia Camargos on 23/02/19.
+//  Copyright © 2019 Ana Leticia Camargos. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class BaseView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configure()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.configure()
+    }
+    
+    func configure ( ) {
+        
+    }
+    
+}
---
+++ Restaurants/Screen/Details/DetailsFoodViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailsFoodViewController.swift
+//  Restaurants
+//
+//  Created by Ana Leticia Camargos on 23/02/19.
+//  Copyright © 2019 Ana Leticia Camargos. All rights reserved.
+//
+
+import UIKit
+
+class DetailsFoodViewController: UIViewController {
+    
+    @IBOutlet weak var detailsFoodView: DetailsFoodView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
