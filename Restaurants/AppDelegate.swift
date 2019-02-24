43b015c897139cb7633fd1c63b720003454c3bb9
Ana <alcvlara@sga.pucminas.br>
2019-02-24 13:20:07 -0300

    Update the List with Restaurants

--- Restaurants/AppDelegate.swift
+++ Restaurants/AppDelegate.swift
@@ -24,38 +24,50 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
         
-        service.request(.search(lat: 42.361145, long: -71.057083)) { (result ) in
-            switch result {
-                case .success(let response):
-                    //print(try? JSONSerialization.jsonObject(with: response.data, options: []) )
-                    let root = try? self.jsonDecoder.decode(Root.self, from: response.data)
-                    print(root)
-                case .failure(let error):
-                    print("Error: \(error)")
-            }
-        }
         
+        
+        // decide tela inicial
         switch locationService.status {
-        case .notDetermined, .denied, .restricted:
-            let locationViewController = storyboard.instantiateViewController(withIdentifier: "LocationViewController") as? LocationViewController
             
-            locationViewController?.locationService = locationService
+            case .notDetermined, .denied, .restricted:
+                let locationViewController = storyboard.instantiateViewController(withIdentifier: "LocationViewController") as? LocationViewController
+                locationViewController?.locationService = locationService
+                window.rootViewController = locationViewController
             
-            window.rootViewController = locationViewController
             
+            default:
+                let nav = storyboard.instantiateViewController(withIdentifier: "RestaurantNavigationController") as? UINavigationController
             
-        default:
-            assertionFailure()
-            //voltar aqui
+                window.rootViewController = nav
+                loadBusinesses()
         }
         
         window.makeKeyAndVisible()
         
         return true
     }
-
     
-
+    private func loadBusinesses( ) {
+        
+        // pega request da api
+        service.request(.search(lat: 42.361145, long: -71.057083)) { (result ) in
+            switch result {
+                
+            case .success(let response):
+                //print(try? JSONSerialization.jsonObject(with: response.data, options: []) )
+                let root = try? self.jsonDecoder.decode(Root.self, from: response.data)
+                let viewModels = root?.businesses.compactMap(RestaurantListViewModel.init)
+                if let nav = self.window.rootViewController as? UINavigationController,
+                    let restaurantListViewController = nav.topViewController as? RestaurantTableViewController {
+                    restaurantListViewController.viewModels = viewModels ?? []
+                }
+                
+            case .failure(let error):
+                print("Error: \(error)")
+            }
+        }
+        
+    }
 
 }
 
